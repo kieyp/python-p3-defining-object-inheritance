0c670d9e632f6e14044be3a2ff3b9ed340ed9525
Boniface <cheruiyotbony@gmail.com>
2024-01-24 10:03:49 +0300

    test test inheritance

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,17 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    
+  def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    
+    
+    
+    
+    
+    
+my_car=Car(wheel_size=1,wheel_number=2)
+
+print(my_car.go())
+
+print(my_car.fill_up_tank())
